ff920e0c1df9916a549950bba90e21cab40b7458
Aaron Mell <aaronamell@gmail.com>
2016-04-17 19:09:25 -0400

    Added docopt for command line arguments.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,47 @@
 extern crate hyper;
+extern crate rustc_serialize;
+extern crate docopt;
 
+use docopt::Docopt;
 mod request;
 use self::request::Request;
 
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+const USAGE: &'static str = "
+Usage:
+  stockfighter checkapi
+  stockfighter -h | --help
+  stockfighter -v | --version
+
+Options:
+  -h, --help  Show this message
+  -v, --version  Show the version of stockfighter
+";
+
+#[derive(Debug, RustcDecodable)]
+struct Args {
+    flag_help: bool,
+    flag_version: bool,
+    cmd_checkapi: bool
+}
+
 fn main() {
 
-    let request = Request {
-        requires_auth: false,
-        request_url: "https://api.stockfighter.io/ob/api/heartbeat"
-    };
+    let args: Args = Docopt::new(USAGE)
+                            .and_then(|d| d.decode())
+                            .unwrap_or_else(|e| e.exit());
 
-    println!("Response: {}", request.send_request());
+    if (args.flag_version)
+    {
+        println!("StockFighter v{}", VERSION)
+    } else if (args.cmd_checkapi)
+    {
+        let request = Request {
+                requires_auth: false,
+                request_url: "https://api.stockfighter.io/ob/api/heartbeat"
+            };
+
+        println!("Response: {}", request.send_request());
+    }
 }
